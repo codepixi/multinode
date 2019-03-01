b50a5264d057226a61f1043f950d4c0fe66983bb
Nadine <accentweb.com+codepixi.github@gmail.com>
2019-03-01 07:15:53 -0500

    Specialisation des types

--- clients/client-html5/allo-multinode/MultiNode.js
+++ clients/client-html5/allo-multinode/MultiNode.js
@@ -7,12 +7,10 @@
     
     function MultiNode()
     {
-    
         try
         {
             this.contact = new WebSocket("ws://127.0.0.1:8080/multinode");
-            //this.contact.onopen = () => contact.send("coucou");
-            this.contact.onopen = () => this.posterVariableTexte("argent", "million");
+            //this.contact.onopen = () => contact.send("allo");
             
             this.contact.onclose = function(evenement)
             {
@@ -29,16 +27,33 @@
         {
             console.log("erreur" + JSON.stringify(erreur));        
         }
-        
-        
-        
-        this.posterVariableTexte = function(id, texte)
+                        
+        this.posterVariableTextuelle = function(id, texte)
         {
+            console.log("MultiNode => posterVariableTextuelle()");
             var variable = new Variable("texte", id, texte);
             this.contact.send(JSON.stringify(variable));
         }
 
+        this.posterVariableNumerique = function(id, nombre)
+        {
+            console.log("MultiNode => posterVariableNumerique()");
+            var variable = new Variable("nombre", id, texte);
+            this.contact.send(JSON.stringify(variable));
+        }
         
+        this.posterVariableBooleenne = function(id, nombre)
+        {
+            console.log("MultiNode => posterVariableBooleenne()");
+            var variable = new Variable("bool", id, texte);
+            this.contact.send(JSON.stringify(variable));
+        }
+        
+        this.recevoirVariable()
+        {
+            
+            
+        }
     
     }
  
