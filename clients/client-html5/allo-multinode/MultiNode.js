fa315e85e02794bd659732e04a3b9ba801c31338
Nadine <accentweb.com+codepixi.github@gmail.com>
2019-03-05 13:58:30 -0500

    Ajustement de protocole client pour distinguer connexion de authentification + hook sur la connexion + retrofit client de demo attaque

--- clients/client-html5/allo-multinode/MultiNode.js
+++ clients/client-html5/allo-multinode/MultiNode.js
@@ -2,106 +2,113 @@
     {
         this.type = type;
         this.cle = cle;
-        this.valeur = valeur;        
+        this.valeur = valeur;
     }
-    
+
     function MultiNode()
-    {
-        
-        this.contact;
-        
+    {        
         var multinode = this;
-        
-        try
-        {
-            this.contact = new WebSocket("ws://127.0.0.1:8080/multinode");
-            //this.contact.onopen = () => contact.send("allo");
+
+        this.messageTransfertVariable = {etiquette:"TRANSFERT_VARIABLE"};
+        this.messageDemandeAuthentification = {etiquette:"DEMANDE_AUTHENTIFICATION"};
+        // messages recus
+        this.messageNotificationAuthentification = {etiquette:"NOTIFICATION_AUTHENTIFICATION"};
+        this.messageNotificationVariable = {etiquette:"NOTIFICATION_VARIABLE"};
+        this.messageConfirmationAuthentification = {etiquette:"CONFIRMATION_AUTHENTIFICATION"};
+
+        multinode.contact;
+        this.connecter = function(pseudonyme){
+
+            try
+            {
+
+                multinode.contact = new WebSocket("ws://127.0.0.1:8080/multinode");
+                //this.contact.onopen = () => contact.send("allo");
+            }
+            catch(erreur)
+            {
+                console.log("erreur" + JSON.stringify(erreur));
+            }
             
-            this.contact.onclose = function(evenement)
+            multinode.contact.onopen = function(evenement)
+            {
+                console.log("ouverture " + JSON.stringify(evenement));
+                multinode.confirmerConnexion(); 
+            }
+
+            multinode.contact.onclose = function(evenement)
             {
                 console.log("fermeture " + JSON.stringify(evenement));
             }
-            
-            this.contact.onmessage = function(evenement) 
+
+            multinode.contact.onmessage = function(evenement)
             {
                 console.log("message " + JSON.stringify(evenement));
                 var message = JSON.parse(evenement.data);
-                
+
                 switch(message.etiquette)
                 {
                     case "NOTIFICATION_CONNEXION":
-                        multinode.apprendreConnexion(message.pseudonyme);
+                        multinode.apprendreAuthentification(message.pseudonyme);
                     break;
                     case "NOTIFICATION_VARIABLE":
-                        var variable = message.variable; 
+                        var variable = message.variable;
                         console.log("variable recue " + variable.valeur);
                         multinode.recevoirVariable(variable);
                     break;
                     case "CONFIRMATION_CONNEXION":
-                        multinode.confirmerConnexion(message.listePseudo);
+                        multinode.confirmerAuthentification(message.listePseudo);
                     break;
                 }
-                
+
                 //console.debug("Message recu", message.valeur);
-            };
-        }
-        catch(erreur)
-        {
-            console.log("erreur" + JSON.stringify(erreur));        
+            };            
+
         }
 
-        this.messageTransfertVariable = {etiquette:"TRANSFERT_VARIABLE"};
-        this.messageDemandeAuthentification = {etiquette:"DEMANDE_AUTHENTIFICATION"};        
-        
         this.posterVariableTextuelle = function(id, texte)
         {
             console.debug("MultiNode => posterVariableTextuelle()",texte);
-            this.messageTransfertVariable.variable = new Variable("texte", id, texte);
-            this.contact.send(JSON.stringify(this.messageTransfertVariable));
+            multinode.messageTransfertVariable.variable = new Variable("texte", id, texte);
+            multinode.contact.send(JSON.stringify(multinode.messageTransfertVariable));
         }
 
         this.posterVariableNumerique = function(id, nombre)
         {
             console.debug("MultiNode => posterVariableNumerique()",nombre);
-            this.messageTransfertVariable.variable = new Variable("numerique", id, texte);
-            this.contact.send(JSON.stringify(this.messageTransfertVariable));
+            multinode.messageTransfertVariable.variable = new Variable("numerique", id, nombre);
+            multinode.contact.send(JSON.stringify(multinode.messageTransfertVariable));
         }
-        
+
         this.posterVariableBooleenne = function(id, booleen)
         {
             console.debug("MultiNode => posterVariableBooleenne()",booleen);
-            this.messageTransfertVariable.variable = new Variable("booleen", id, texte);
-            this.contact.send(JSON.stringify(this.messageTransfertVariable));
+            multinode.messageTransfertVariable.variable = new Variable("booleen", id, booleen);
+            multinode.contact.send(JSON.stringify(multinode.messageTransfertVariable));
         }
-        
+
         this.demanderAuthentification = function(pseudonyme)
         {
             console.debug("MultiNode => demanderAuthentification()",pseudonyme);
-            this.messageDemandeAuthentification.pseudonyme = pseudonyme;
-            this.contact.send(JSON.stringify(this.messageDemandeAuthentification));
-            
+            multinode.messageDemandeAuthentification.pseudonyme = pseudonyme;
+            multinode.contact.send(JSON.stringify(multinode.messageDemandeAuthentification));            
         }
-
-        // messages recus
-        this.messageNotificationConnexion = {etiquette:"NOTIFICATION_CONNEXION"};
-        this.messageNotificationVariable = {etiquette:"NOTIFICATION_VARIABLE"};
-        this.messageConfirmationConnexion = {etiquette:"CONFIRMATION_CONNEXION"};
 
         // fonction a redefinir
+        this.confirmerConnexion = function()
+        {
+        }
+
+        this.confirmerAuthentification = function(autresParticipants)
+        {
+        }
+        
+        this.apprendreAuthentification = function(pseudonyme)
+        {
+        }
+        
         this.recevoirVariable = function(variable)
         {
-            
-            
         }
         
-        this.apprendreConnexion = function(pseudonyme)
-        {
-            
-        }
-        
-        this.confirmerConnexion = function(participants)
-        {
-            
-        }
     }
- 
